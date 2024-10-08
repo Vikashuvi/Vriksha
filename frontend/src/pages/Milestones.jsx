39a5290b18d41ee7103c3fab21cceee09666e365
Vikashuvi <vikashuvi07@gmail.com>
2024-10-08 15:58:35 +0530

    milestone issue solved

--- frontend/src/pages/Milestones.jsx
+++ frontend/src/pages/Milestones.jsx
@@ -13,12 +13,25 @@ const MilestonesSection = styled.section`
 
 const StickyContainer = styled.div`
   position: relative;
+
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem 0; // Reduced padding
+  }
 `;
 
 const ImageContainer = styled.div`
   position: sticky;
   top: ${props => props.top};
   width: 100%;
+
+  @media (max-width: 768px) {
+    position: static;
+    width: 100%; // Changed from 90% to 100%
+    margin-bottom: 1rem; // Reduced margin
+  }
 `;
 
 const Figure = styled.figure`
@@ -27,7 +40,12 @@ const Figure = styled.figure`
   display: flex;
   align-items: center;
   justify-content: center;
-  position: relative; // Add this line
+  position: relative;
+
+  @media (max-width: 768px) {
+    height: 100%;
+    
+  }
 `;
 
 const MilestoneText = styled.h2`
@@ -37,6 +55,12 @@ const MilestoneText = styled.h2`
   color: white;
   text-transform: uppercase;
   z-index: 1;
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+    position: static;
+   
+  }
 `;
 
 const Image = styled.img`
@@ -44,13 +68,19 @@ const Image = styled.img`
   height: 80%;
   width: ${props => props.width};
   object-fit: cover;
-  border-radius: 0.375rem; // Equivalent to rounded-md
-  display: ${props => props.src ? 'block' : 'none'}; // Add this line
+  border-radius: 0.375rem;
+  display: ${props => props.src ? 'block' : 'none'};
+
+  @media (max-width: 768px) {
+    width: 90%; // Changed from 100% to 90%
+    height: auto; // Changed from 100% to auto
+    aspect-ratio: 1 / 1; // Added to maintain a square aspect ratio
+  }
 `;
 
 const Milestones = () => {
   const images = [
-    { src: '', width: '55%', top: '0' }, // Empty first image
+    { src: '', width: '50%', top: '0' },
     { src: mil1, width: '55%', top: '0' },
     { src: mil2, width: '60%', top: '2rem' },
     { src: mil3, width: '65%', top: '4rem' },
@@ -64,7 +94,7 @@ const Milestones = () => {
           <ImageContainer key={index} top={image.top}>
             <Figure>
               {index === 0 && <MilestoneText>Milestone</MilestoneText>}
-              {image.src && ( // Add this condition
+              {image.src && (
                 <Image 
                   src={image.src} 
                   alt={`Milestone ${index + 1}`} 
